ccc448c934e8d3ca46e35b968fa8234cbab6d721
ArySanatan <sanatan14121@gmail.com>
2025-01-03 18:38:31 +0530

    Update 3.py
    done done yes

--- 3.py
+++ 3.py
@@ -1,3 +1,16 @@
 '''3. Python program to print all prime numbers from 1 to 100.'''
-#not solved - logic buiding issue
-def isprime(number):
+
+#very good logic
+
+print("All Prime Numbers from 1 to 100 are : ")
+n=2
+
+while(n<=100):
+    k=2
+    for i in range (1,n+1):
+        if (n%i !=0 and n%1==0): 
+            k=k+1
+            
+    if (n==k):print(n,end="  ")        
+    n+=1
+       
