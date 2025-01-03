2d177f5d6557bb9acf9ba2406d30ba708447ad01
ArySanatan <sanatan14121@gmail.com>
2025-01-03 17:15:49 +0530

    Update 10.py
    Problem solved - swap and sort

--- 10.py
+++ 10.py
@@ -1,19 +1,17 @@
 '''10. Write a Python program to get the smallest number from a list without using any builtin
 function.'''
-##not solved - problem in logic buiding
+#solved - sorting
 
-list=[45,25,36,84,1,254,789,9,56,85,23,78]
+list=[45,25,36,84,1,254,789,9,56,85,-1,23,78]
 
 
 n=0
-i=0
-while(n<=(len(list)-1)):
-    freeze=list[n]
+while(n<len(list)-1):
+        if list[n] < list[n+1]: 
+            c=list[n]
+            list[n]=list[n+1]
+            list[n+1] = c
+        n+=1
     
-    while(i<=(len(list)-1)):
-        checker=list[i]
-        if (freeze>checker): list.remove(freeze)
-        else: list.remove(checker)
-        i=i+1
-    n=n+1
-print(list)
+
+print("Smallest Number from the list is ",list[-1])
